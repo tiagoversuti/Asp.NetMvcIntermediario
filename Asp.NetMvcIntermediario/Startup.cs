62f53891675f36a33651f794e0dddc9e18cdbd25
tiagoversuti <tiagoversuti@gmail.com>
2016-10-04 16:32:55 -0300

    Add project files.

---
+++ Asp.NetMvcIntermediario/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Asp.NetMvcIntermediario.Startup))]
+namespace Asp.NetMvcIntermediario
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
