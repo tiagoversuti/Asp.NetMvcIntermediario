dcbcc5ec53a7f207cb5471dbeb3761f4aea209c6
tiagoversuti <tiagoversuti@gmail.com>
2016-10-06 16:19:55 -0300

    Tópicos: - Customizar uma View - Customizar o Controller e a View

---
+++ Asp.NetMvcIntermediario/Controllers/TesteController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Asp.NetMvcIntermediario.Models;
+
+namespace Asp.NetMvcIntermediario.Controllers
+{
+    public class TesteController : Controller
+    {
+        private BancoContexto db = new BancoContexto();
+
+        // GET: Teste
+        public ActionResult Index()
+        {
+            return View(db.Alunos.ToList());
+        }
+
+        public ViewResult Details(int id)
+        {
+            Aluno aluno = db.Alunos.Find(id);
+            return View(aluno);
+        }
+    }
+}
